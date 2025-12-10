9af7daf4c766c7e27c89945fe300d5190ef93f97
Edson <edin2008@yahoo.com.br>
2025-12-09 23:06:42 -0300

    Criando componente de botao buscar

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,12 +3,18 @@ import gitLogo from '../assets/github.png';
 import  Input  from '../components/Input';
 import { Container } from './styles';
 import ItemRepo from '../components/ItemRepo';
+import { useState } from 'react';
+import Button from '../components/Button';
 
 function App() {
+
+  const [repos, setRepos] = useState([]);
+
   return (
     <Container>
      <img src={gitLogo} width={72} height={72} alt='Logo do github'/>
      <Input />
+     <Button />
      <ItemRepo />
     </Container>
   );
