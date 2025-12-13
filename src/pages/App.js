4dd43bd4cc3ea6dd6f469fb91196da11198b79b9
Edson <edin2008@yahoo.com.br>
2025-12-12 22:41:02 -0300

    adicionando função de remover repositorio e uma validação

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,17 +5,43 @@ import { Container } from './styles';
 import ItemRepo from '../components/ItemRepo';
 import { useState } from 'react';
 import Button from '../components/Button';
+import {api} from '../services/api';
+
 
 function App() {
 
   const [repos, setRepos] = useState([]);
+  const [currentRepo, setCurrentRepo] = useState('');
+
+  const handleSearchRepo = async () => {
+    try {
+      const {data} = await api.get(`repos/${currentRepo}`)
+
+      if (data.id) {
+        const isExist = repos.find(repo => repo.id === data.id);
+        if (!isExist) {
+        setRepos(prev => [...prev, data]);
+        setCurrentRepo('');
+        return ;
+        }
+    }
+    } catch (error) {
+        alert('Repositorio não encontrado');
+    }
+    
+  }
+
+  const handleRemoveRepo = (id) => {
+    const removeRepo = repos.filter(repo => repo.id !== id);
+    setRepos(removeRepo);
+  }
 
   return (
     <Container>
      <img src={gitLogo} width={72} height={72} alt='Logo do github'/>
-     <Input />
-     <Button />
-     <ItemRepo />
+     <Input value={currentRepo} onChange={(e) => setCurrentRepo(e.target.value)}/>
+     <Button onClick={handleSearchRepo}/>
+     {repos.map(repo => <ItemRepo handleRemoveRepo={handleRemoveRepo} repo ={repo}/>)}
     </Container>
   );
 }
